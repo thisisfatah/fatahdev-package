dd7e9468820af7af1f103d5e6247727585d2ba68
thisisfatah <48764326+thisisfatah@users.noreply.github.com>
2024-12-05 16:35:26 +0700

    assembely edit

--- Runtime/Core/Transition.cs
+++ Runtime/Core/Transition.cs
@@ -1,7 +1,3 @@
-// Copyright (C) 2017 gamevanilla. All rights reserved.
-// This code can only be used under the standard Unity Asset Store End User License Agreement,
-// a copy of which is available at http://unity3d.com/company/legal/as_terms.
-
 using System.Collections;
 
 using UnityEngine;
